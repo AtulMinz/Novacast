2fa8f00c61c5c9a5b1989c7463a3dc2123cd4a5c
AtulMinz <atulminz.jsr@gmail.com>
2024-03-24 18:50:39 +0530

    Created streaming screen

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -7,9 +7,9 @@ const Hero = () => {
     navigate("/stream");
   };
   return (
-    <main className="bg-black min-h-screen justify-center flex p-[30vh]">
+    <main className="min-h-screen justify-center flex p-[30vh]">
       <div className="flex flex-col items-center">
-        <h1 className="text-gray-300 font-bold text-6xl">
+        <h1 className="text-gray-200 font-bold text-6xl">
           Welcome to Novacast
         </h1>
         <p className="text-gray-300 mt-3 text-2xl">
--- client/src/components/Stream.tsx
+++ client/src/components/Stream.tsx
@@ -1,10 +1,34 @@
-import React from "react";
-
 const Stream = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <main className="flex justify-center items-center min-h-screen">
+      <div className="card w-[35vw] bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Novacast</h2>
+          <p>Stream your victory</p>
+          <div className="card-actions justify-between">
+            <label className="input input-bordered flex items-center gap-2">
+              Stream URL
+              <input type="text" className="grow" placeholder="YouTube RTMP" />
+            </label>
+            <label className="input input-bordered flex items-center gap-2">
+              Stream key
+              <input
+                type="password"
+                className="grow"
+                placeholder="YouTube Stream key"
+              />
+            </label>
+            <button className="btn btn-primary">Start Stream</button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
